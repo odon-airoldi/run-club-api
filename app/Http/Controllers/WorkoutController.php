57ea97142e630f8f72be0b291fae7fb8841f81a0
odon-airoldi <odon.airoldi@gmail.com>
2026-07-19 13:25:20 +0200

    Add workout creation form and implement store functionality in WorkoutController

--- app/Http/Controllers/WorkoutController.php
+++ app/Http/Controllers/WorkoutController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\Workout;
 use Illuminate\Http\Request;
 
@@ -22,15 +23,33 @@ class WorkoutController extends Controller
      */
     public function create()
     {
-        //
+        return view('workout.workout-create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request, User $user)
     {
-        //
+
+
+        $data = $request->all();
+        [$minutes, $seconds] = explode(':', $data['pace']);
+        $pace = ($minutes * 60) + $seconds;
+
+        $newWorkout = new Workout();
+        $newWorkout->name = $data['name'];
+        $newWorkout->description = $data['description'];
+        $newWorkout->date_time = $data['date'];
+        $newWorkout->place_city = $data['place_city'];
+        $newWorkout->place_address = $data['place_address'];
+        $newWorkout->buffer_time = $data['buffer_time'];
+        $newWorkout->distance = $data['distance'];
+        $newWorkout->pace = $pace;
+        // $newWorkout->user_id = $data[''];
+        $newWorkout->save();
+
+        return redirect()->route('workouts.show', $newWorkout);
     }
 
     /**
