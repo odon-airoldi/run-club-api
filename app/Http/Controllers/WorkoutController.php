51c18f26d6221a959b8358d7cee465a3776345ed
odon-airoldi <odon.airoldi@gmail.com>
2026-07-20 11:11:46 +0200

    Refactor destroy method in WorkoutController to accept Workout model instance; implement deletion and redirect to workouts index

--- app/Http/Controllers/WorkoutController.php
+++ app/Http/Controllers/WorkoutController.php
@@ -99,8 +99,10 @@ class WorkoutController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Workout $workout)
     {
-        //
+        $workout->delete();
+
+        return redirect()->route('workouts.index');
     }
 }
