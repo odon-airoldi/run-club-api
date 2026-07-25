c52dcf8bd65de15c01ffff8fed3f88487f1b137e
odon-airoldi <odon.airoldi@gmail.com>
2026-07-25 15:43:35 +0200

    Add workouts resource route with authentication middleware

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,6 @@ Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
 
-require __DIR__.'/auth.php';
+Route::resource('workouts', WorkoutController::class)->middleware(['auth', 'verified']);
+
+require __DIR__ . '/auth.php';
