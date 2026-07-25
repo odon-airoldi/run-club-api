55a8a94a4b19793bd3606488340f495cecdb3d58
odon-airoldi <odon.airoldi@gmail.com>
2026-07-25 15:48:10 +0200

    Add migration for workouts table with necessary fields

---
+++ database/migrations/2026_07_25_134725_create_workouts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('workouts', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name');
+            $table->text('description');
+            $table->dateTime('date_time');
+            $table->string('place_city');
+            $table->string('place_address');
+            $table->unsignedTinyInteger('buffer_time');
+            $table->unsignedTinyInteger('distance');
+            $table->unsignedSmallInteger('pace');
+            $table->foreignId('user_id')->constrained();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('workouts');
+    }
+};
