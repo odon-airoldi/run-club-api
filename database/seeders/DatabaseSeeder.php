7abb0d5d69e7db75cf40bd067b175efd67351008
odon-airoldi <odon.airoldi@gmail.com>
2026-09-02 14:36:22 +0200

    Refactor user model to replace 'name' with 'first_name' and 'last_name' in factory and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,8 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
+            'first_name' => 'Test first name',
+            'last_name' => 'Test last name',
             'email' => 'test@example.com',
         ]);
 
