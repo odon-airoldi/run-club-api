e8c84b8b32b1e426c43c66c67d02ba3b15808d85
odon-airoldi <odon.airoldi@gmail.com>
2026-09-02 12:40:46 +0200

    Add picture field to user model and implement accessor for picture attribute

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,16 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable(['name', 'email', 'picture', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
@@ -20,14 +22,25 @@ class User extends Authenticatable
     /**
      * Get the attributes that should be cast.
      *
-     * @return array<string, string>
+     * @return array <string, string>
      */
+
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    // accessor - trasformo picture in lettura dal model, senza modificare il valore del db
+    protected function picture(): Attribute
+    {
+        return Attribute::make(
+            // get è una funzione eseguita quando accedi a $user->picture che riceve come valore grezzo quello del db
+            // funzioen con parametro value, se value ha un valore trasformalo altrimenti null
+            get: fn($value) => $value ? asset('storage/' . $value) : null,
+        );
     }
 
     public function workouts()
