8cad971b1d984a8b60b854e522ce524f9652884b
odon-airoldi <odon.airoldi@gmail.com>
2026-09-10 18:54:54 +0200

    Refactor middleware configuration to enable stateful API handling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,10 +13,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->api(prepend: [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-        ]);
-        // $middleware->statefulApi();
+        // $middleware->api(prepend: [
+        //     \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+        // ]);
+        $middleware->statefulApi();
 
         $middleware->alias([
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
